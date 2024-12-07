3ea960f8561c86c5b0ebabca4ebb962f1e92a5c0
Crowdbotics <team@crowdbotics.com>
2024-12-07 08:52:25 +0000

    Initial commit

---
+++ backend/mynovelist_50251/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for mynovelist_50251 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "mynovelist_50251.settings"
+)
+
+application = get_wsgi_application()
